d7111b8d0473a072768048a025ce4a48abb9394a
Andreea Calin <andreea.calin@vtex.com.br>
2021-12-08 17:27:27 +0200

    MR feedback

--- react/DeliverySettings.tsx
+++ react/DeliverySettings.tsx
@@ -246,39 +246,42 @@ const DeliverySettings: FC = () => {
   }
 
   const handleSubmit = async (showToast: any) => {
-    setIsLoading(true);
-    if (freeDeliveryThreshold !== 0) {
-      handlePromotionSubmit()
-    }
-
-    await updateShippingRate({variables: {
-      ...shippingRate
-    }}).catch(err=>{
-      console.error(err)
-    })
-
-    await updateOnboarding({
-      variables: {
-        shippingCharge: parseFloat(shippingRate.input[0].absoluteMoneyCost),
-        freeDeliveryThreshold: freeDeliveryThreshold !== 0 && isActive ? freeDeliveryThreshold.toString() : ""
+    try {
+      setIsLoading(true);
+      if (freeDeliveryThreshold !== 0) {
+        handlePromotionSubmit()
       }
-    }).catch(err=>{
-      console.error(err)
-    })
 
-    await updateShippingPolicy({variables: {
-      ...shippingPolicy
-    }}).catch(err => {
-      setIsLoading(false);
-      console.error(err)
-      showToast({
-        message: intl.formatMessage({
-          id: 'admin/shipping-policy.toast.failure',
-        }),
-        duration: 5000,
+      await updateShippingRate({
+        variables: {
+          ...shippingRate
+        }
       })
-    })
-    .then(() => {
+
+      await updateOnboarding({
+        variables: {
+          shippingCharge: parseFloat(shippingRate.input[0].absoluteMoneyCost),
+          freeDeliveryThreshold: freeDeliveryThreshold !== 0 && isActive ? freeDeliveryThreshold.toString() : ""
+        }
+      })
+      await updateShippingPolicy({
+        variables: {
+          ...shippingPolicy
+        }
+      }).catch(err => {
+        setIsLoading(false);
+        console.error(err)
+        showToast({
+          message: intl.formatMessage({
+            id: 'admin/shipping-policy.toast.failure',
+          }),
+          duration: 5000,
+        })
+      })
+    } catch (err) {
+      console.error(err);
+    }
+    finally {
       setIsLoading(false);
       showToast({
         message: intl.formatMessage({
@@ -286,7 +289,7 @@ const DeliverySettings: FC = () => {
         }),
         duration: 5000,
       })
-    })
+    }
   }
 
   const handlePromotionSubmit = () => {
